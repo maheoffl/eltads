f77a0003129516fcb65e596eeabe0b43395c6bff
maheoffl <chmahesh997@gmail.com>
2024-03-10 11:21:35 +0530

    fea:  added footer page with google maps

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React from 'react';
+import Maps from './Maps';
+import home from '../assets/homeicon.png';
+import callicon from '../assets/call-icon.png';
+import emailicon from '../assets/email icon.png';
 
 function Footer() {
   return (
-      <div className=' h-[70vh] w-full bg-blue-100'>
-          hello
+    <>
+      <div className=' h-[70vh] w-full flex  items-center px-28 justify-between text-white font-mono ' style={{backgroundColor:"#001C40"}}>
+        <Maps />
+        <div className="leftfoot h-[80%] w-[50%]   flex flex-col gap-10  ">
+          <h2 className=' text-2xl font-semibold h-[10%]'>Contact</h2>
+          <div className="address h-[20%] px-10 flex gap-10 p">
+            <div className="lhome">
+              <img src={home} alt="home"  className=' h-10 invert'/>
+            <p>Address <br /> </p>
+            </div>
+            <div className="rhome pt-4">
+              <p> street no 3 ,Ramanthapur ,hyderabad <br /> Telangana 500044</p>
+            </div>
+
+          </div>
+          <div className="email h-[20%] px-10 flex gap-10">
+             <div className="lhome">
+              <img src={callicon} alt="home" className=' h-10 invert' />
+            <p>Call us<br /> </p>
+            </div>
+            <div className="rhome pt-4 ">
+              <p>+91 9666160666</p>
+            </div></div>
+          <div className="phone h-[30%] px-10 flex gap-10">
+            <div className="lhome">
+              <img src={emailicon} alt="email" className=' h-10 invert' />
+            <p>E-Mail <br /> </p>
+            </div>
+            <div className="rhome pt-4">
+              <p>eltautoads@gmail.com</p>
+            </div></div>
+        </div>
+        
     </div>
+    </>
   )
 }
 
