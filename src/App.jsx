cb4436208c78950afd842103d53945187b811671
maheoffl <chmahesh997@gmail.com>
2024-03-11 13:47:27 +0530

    feature: added responsive breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import Header from './components/Header';
-import Carosel from './components/Carosel';
-import Aboutus from './components/Aboutus';
-import Whyus from './components/Whyus';
-import Testimonials from './components/Testimonials';
-import Footer from './components/Footer';
-import Head from './components/Head';
+import React from "react";
+import Carosel from "./components/Carosel";
+import Aboutus from "./components/Aboutus";
+import Whyus from "./components/Whyus";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+import Head from "./components/Head";
 
 const App = () => {
   return (
     <>
-     <Head/>
+      <Head />
       <Carosel />
       <Aboutus />
       <Whyus />
       <Testimonials />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
